3a66d95d630a390fadef0d40d5296ec53fd7d35b
git-fagun <git1-fagun@devxhub.com>
2025-02-10 11:26:59 +0600

    Saving local changes before rebase

--- src/components/Character/utils/lighting.ts
+++ src/components/Character/utils/lighting.ts
@@ -23,8 +23,8 @@ const setLighting = (scene: THREE.Scene) => {
     .load("char_enviorment.hdr", function (texture) {
       texture.mapping = THREE.EquirectangularReflectionMapping;
       scene.environment = texture;
-      scene.environmentIntensity = 0;
-      scene.environmentRotation.set(5.76, 85.85, 1);
+      (scene.environment as any).intensity = 0;
+      (scene.environment as any).rotation.set(5.76, 85.85, 1);
     });
 
   function setPointLight(screenLight: any) {
